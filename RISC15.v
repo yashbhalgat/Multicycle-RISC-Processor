a7f0d8526c7e223d735c88b15220ccba58261420
meetshah1995 <meetshah1995@iitb.ac.in>
2015-10-22 11:14:08 +0530

    Add simulation folder

--- RISC15.v
+++ RISC15.v
@@ -24,3 +24,21 @@ module RISC15(clk);
 	memory 		 __mem(T1out, Mux9_memDataIn_out, memDataOut, Mux8_memwrite_out, memread, clk);
 
 endmodule
+
+module RISC15_test();
+
+	reg [5:0]  T1out;
+	reg        clk;
+	reg [15:0] memIn;
+	initial 
+		begin
+			clk <= 1'b1;
+			T1out <= 6'd0;
+			Mux4_RF_wadd <= 3'b000;
+			IR <= 16'b0000001010100000;
+			Mux3_RF_wen <= 2'b00;
+		end
+
+	always #5 clk = ~clk;
+
+endmodule
