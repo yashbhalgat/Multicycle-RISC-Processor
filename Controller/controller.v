efc018657eb1f21dd16bc9e78cb42b128c04d049
meetshah1995 <meetshah1995@iitb.ac.in>
2015-10-22 22:26:34 +0530

    Add BEQ with Testing

--- Controller/controller.v
+++ Controller/controller.v
@@ -243,12 +243,12 @@ module controller(clk, proc_rst, compare, IR, Mux1_alu_B, Mux2_alu_A, Mux3_RF_we
 					18:begin
 						memread <= 0;
 						CZ_en <= 1;
-					end
-
-					19:begin
 						Mux6_RF_dataIn <= 1'b0;
 						Mux3_RF_wen <= 2'b00;
 						Mux4_RF_wadd <= 3'b000;
+					end
+
+					19:begin
 					end
 
 					20:begin
@@ -280,6 +280,7 @@ module controller(clk, proc_rst, compare, IR, Mux1_alu_B, Mux2_alu_A, Mux3_RF_we
 					22:begin
 						Mux2_alu_A <= 3'b101;
 						Mux1_alu_B <= 3'b010;
+						Mux5_RF_read2 <= 2'b00;
 						CZ_en <= 1;
 						ALU_op <= 0;					
 					end
@@ -296,6 +297,7 @@ module controller(clk, proc_rst, compare, IR, Mux1_alu_B, Mux2_alu_A, Mux3_RF_we
 						Mux1_alu_B <= 3'b010;
 						Mux2_alu_A <= 3'b011;
 						Mux5_RF_read2 <= 2'b10;
+						T1write <= 1'b0;
 						CZ_en <= 1;
 						ALU_op <= 0;
 					end
@@ -468,7 +470,7 @@ module controller(clk, proc_rst, compare, IR, Mux1_alu_B, Mux2_alu_A, Mux3_RF_we
 					  end
 					16: StateID=17;
 					17: StateID=3;
-					18: StateID=19;
+					18: StateID=3;
 					19: StateID=3;
 					20: StateID=21;
 					21: StateID=3;
